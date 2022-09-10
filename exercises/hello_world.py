997d071674734ab126487855b8c71fb1ccdaa70f
cmendez20 <cmendez20@utexas.edu>
2022-09-10 16:52:25 -0500

    Initial commit

---
+++ exercises/hello_world.py
@@ -0,0 +1,10 @@
+message = "Hello Python World!"
+print (message)
+
+# print("Hello Python World!")
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+message = "Hello Python Crash Course reader"
+print(message)
---
+++ exercises/simple_message.py
@@ -0,0 +1,5 @@
+message = "Hello, how are you doing?"
+print(message)
+
+message = "I am fine, thank you for asking."
+print(message)
